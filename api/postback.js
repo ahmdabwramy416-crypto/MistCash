f7f77250559bc8d508f2bd4a03ef990df940a42d
ahmdabwramy416-crypto <ahmdabwramy416@gmail.com>
2026-09-16 14:11:05 +0300

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
     }
 
     await userRef.update({
-      balance: FieldValue.increment(payout)
+      points: FieldValue.increment(payout)
     });
 
     return res.status(200).send('OK');
